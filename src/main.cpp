3f05747eb0674ef49993091357d072d6e976c497
pedrolima2806 <pedrolimasilva2003@gmail.com>
2026-04-19 20:42:44 -0300

    Inicialização e imagens

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <string>
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_mixer/SDL_mixer.h>
+
+
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    //Initialization
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    if (!MIX_Init()) {
+        std::cerr << "MIX_Init failed." << std::endl;
+    }
+
+    //Window e renderer
+    int width = 640, height = 480;
+    SDL_Window *window = SDL_CreateWindow("hellcat_mp", width, height, 0);
+    if (!window) {
+        std::cerr << "Failed to create window:" << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        std::cerr << "Failed to create renderer:" << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
